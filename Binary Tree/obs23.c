6db785e5d82ce7c0b1b5084c06216d9c12bf4573
ShivaPrakash-006 <shivapraksh2006@gmail.com>
2025-04-02 21:47:38 +0530

    Binary Tree - Creation and Traversal

---
+++ Binary Tree/obs23.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+  int data;
+  struct Node *left;
+  struct Node *right;
+} Node;
+
+Node *create(void) {
+  int x;
+  scanf("%i", &x);
+  if (x == -1)
+    return NULL;
+  Node *ptr = (Node *)malloc(sizeof(Node));
+  ptr->data = x;
+  printf("Left of %i: ", x);
+  ptr->left = create();
+  printf("Right of %i: ", x);
+  ptr->right = create();
+  return ptr;
+}
+
+void inOrder(Node *root) {
+  if (root == NULL)
+    return;
+  inOrder(root->left);
+  printf("%i ", root->data);
+  inOrder(root->right);
+}
+
+void postOrder(Node *root) {
+  if (root == NULL)
+    return;
+  postOrder(root->left);
+  postOrder(root->right);
+  printf("%i ", root->data);
+}
+
+void preOrder(Node *root) {
+  if (root == NULL)
+    return;
+  printf("%i ", root->data);
+  preOrder(root->left);
+  preOrder(root->right);
+}
+
+int main() {
+  printf("Root: ");
+  Node *root = create();
+  printf("In Order Traversal: ");
+  inOrder(root);
+  printf("\n");
+  printf("Pre Order Traversal: ");
+  preOrder(root);
+  printf("\n");
+  printf("Post Order Traversal: ");
+  postOrder(root);
+  printf("\n");
+
+  return 0;
+}
